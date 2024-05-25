a6e87d1b6efeee1e3d6d5cc10d1067c179f3be76
taeng <xodn0720@naver.com>
2024-05-25 23:14:27 +0900

    Update background.js

--- Js/background.js
+++ Js/background.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const start = 700; // 600px 만큼 내려갔을 때 버튼이 나타나도록 하기 위한 변수 선언
+    const start = 820; // 600px 만큼 내려갔을 때 버튼이 나타나도록 하기 위한 변수 선언
     const collectBtn = document.querySelector('.div_scroll'); // HTML에서 적용될 대상 불러오기 
 
     window.addEventListener('scroll', function () { // scroll 이벤트가 발생했을 때
