dc9607b9a24647839540821905751d9ee05da22c
taeng <xodn0720@naver.com>
2024-05-26 22:10:55 +0900

    update

--- Js/background.js
+++ Js/background.js
@@ -1,20 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
     const start = 820; // 600px 만큼 내려갔을 때 버튼이 나타나도록 하기 위한 변수 선언
     const collectBtn = document.querySelector('.div_scroll'); // HTML에서 적용될 대상 불러오기 
+    const nav_R = document.querySelector('.nav_right'); // HTML에서 적용될 대상 불러오기 
+    const icon = document.querySelector('.mdi--weather-night');
 
     window.addEventListener('scroll', function () { // scroll 이벤트가 발생했을 때
         const scrollTop = window.pageYOffset; // 스크롤된 값을 이용하기 위한 변수 선언
 
         if (scrollTop > start) { // 스크롤 탑의 값이 start보다 클 경우 -> 스크롤을 일정 범위 이상 내렸을 때
-            collectBtn.style.borderBottom = "1px solid black"; // 요소를 보이게 함
-            collectBtn.style.backgroundColor = "rgb(245, 245, 245)";
+            collectBtn.style.borderBottom = "1px solid #dadada"; // 요소를 보이게 함
+            collectBtn.style.backgroundColor = "rgb(252, 252, 252)";
             collectBtn.style.display = "flex";
             collectBtn.style.position = "sticky";
+            nav_R.style.paddingRight = "10px";
+            icon.style.paddingRight = "-40px";
+            icon.style.opacity = "1";
         } else {
             collectBtn.style.borderBottom = ""; 
             collectBtn.style.backgroundColor = ""; 
             collectBtn.style.display = "";
             collectBtn.style.position = "";
+            nav_R.style.paddingRight = "";
+            icon.style.paddingRight = "";
+            icon.style.opacity = "0";
         }
     });
 
